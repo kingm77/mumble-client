e3d0dcb6d6e0c3381beb8e7a6c10ae179887b6b5
MAVOUNGOU KING Steve Jordan <smavoungouking@murex.com>
2022-08-25 10:17:50 +0200

    remove react-cookie

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import { ApolloProvider } from '@apollo/client';
 import React from 'react';
-import { CookiesProvider } from 'react-cookie';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from "react-router-dom";
 import App from './App';
@@ -13,13 +12,11 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-        <CookiesProvider>
             <HashRouter>
                 <ApolloProvider client={backendClient} >
                         <App />
                 </ApolloProvider>
             </HashRouter>
-        </CookiesProvider>
   </React.StrictMode>
 );
 
